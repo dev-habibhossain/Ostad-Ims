2eb2aba5432630a2a6e9390b173e11550ce28d15
Habib Hossain <habibfahim71@gmail.com>
2026-01-27 22:54:16 +0300

    db

--- database/migrations/2026_01_26_210157_create_products_table.php
+++ database/migrations/2026_01_26_210157_create_products_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->string('image');
+            $table->integer('quantity');
+            $table->string('sku');
             $table->timestamps();
         });
     }
